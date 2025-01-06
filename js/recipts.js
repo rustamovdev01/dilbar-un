df7d34820f92721973f7fd801f6c85f37b3b221b
Mirshod <rmirshoddev96@gmail.com>
2025-01-06 16:14:53 +0500

    dilbar-un

--- js/recipts.js
+++ js/recipts.js
@@ -8,3 +8,46 @@ document.querySelectorAll('.option').forEach(function (option) {
 		document.querySelector('.options').style.display = 'none'
 	})
 })
+
+let firstBtn = document.querySelector('.input-btn'),
+	firstBtnFront = document.querySelector('.input-btn-front'),
+	secondBtn = document.querySelector('.offer'),
+	secondBtnFront = document.querySelector('.offer-front')
+
+firstBtn.addEventListener('mouseenter', () => {
+	firstBtnFront.style = `
+		width: 100%;
+	height: 48px;
+	transition: 0.4s ease-in-out;
+	background-color: #502f0f;
+	border-radius: 30px;
+	`
+})
+
+firstBtn.addEventListener('mouseleave', () => {
+	firstBtnFront.style = `
+		width: 10px;
+	height: 10px;
+	transition: 0.4s ease-in-out;
+	border-radius: 50%;
+	`
+})
+
+secondBtn.addEventListener('mouseenter', () => {
+	secondBtnFront.style = `
+		width: 100%;
+	height: 48px;
+	transition: 0.4s ease-in-out;
+	background-color: #502f0f;
+	border-radius: 30px;
+	`
+})
+
+secondBtn.addEventListener('mouseleave', () => {
+	secondBtnFront.style = `
+		width: 10px;
+	height: 10px;
+	transition: 0.4s ease-in-out;
+	border-radius: 50%;
+	`
+})
